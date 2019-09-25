81a58912642c5256cd9100c861dc8b959a284972
Dimas Agung Yudono <dimasday90@gmail.com>
2019-09-25 23:22:42 +0700

    exercise 7 revised and done

--- exercise-7-asterisk.js
+++ exercise-7-asterisk.js
@@ -1,4 +1,4 @@
-// Barisan Bintang
+1. // Barisan Bintang
 
 console.log('barisan bintang 1')
 var rows1 = '*';
@@ -9,28 +9,26 @@ for(i = 0; i < 5; i ++) {
 2. // Barisan Bintang dengan Nested Looping
 
 console.log('barisan bintang 2')
-var rows3 = '*'
-var tampungan = ''
-for(i = 0; i < 5; i ++) {
-     for(i = 0; i < 5; i ++) {
-         tampungan = tampungan + rows3;
-    }  
+var rows2 = '*';
+var tampungan = '';
+
+for(i= 0; i < 5; i ++) {
+    for(j = 0; j < 5; j ++) {
+        tampungan = tampungan + rows2;
+    }
     console.log(tampungan);
-    console.log(tampungan);
-    console.log(tampungan);
-    console.log(tampungan);
-    console.log(tampungan);
+    tampungan = '';
 }
 
 3. // Barisan tanggan bintang dengan nested loop
 
 console.log('barisan tangga bintang')
-var pagar = '*****';
+var pagar = '*';
 var tampungan2 = '';
 
-for(i= 0; i < pagar.length; i ++) {
-    for(i = 0; i < pagar.length; i ++) {
-        tampungan2 = tampungan2 + pagar[i]
-        console.log(tampungan2)
+for(i= 0; i < 5; i ++) {
+    for(i = 0; i < 5; i ++) {
+        tampungan2 = tampungan2 + pagar;
+        console.log(tampungan2);
     }
 }
