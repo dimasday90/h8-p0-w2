8fe9d48875eb9c6fc901e911899cffbf0b6642f0
Dimas Agung Yudono <dimasday90@gmail.com>
2019-09-26 22:07:54 +0700

    revised exercise 9

--- exercise-9-function.js
+++ exercise-9-function.js
@@ -13,14 +13,14 @@ var hasilPerkalian = calculateMultiply(num1, num2);
 console.log(hasilPerkalian);
 
 function processSentence() {
-    return "Nama saya " + name + ", umur saya " + age + " tahun, alamat saya di " + address + ", dan saya punya hobby yaitu " + hobby + " dan " + hobby2
+    return "Nama saya " + name + ", umur saya " + age + " tahun, alamat saya di " + address + ", dan saya punya hobby yaitu " + hobby
 }
 
 var name = 'Dimas';
 var age = 22;
 var address = 'Jln. Panji Wangko no.8, Mataram';
-var hobby = 'gaming';
-var hobby2 = 'watching anime and other pop-culture';
+var hobby = 'gaming and watching anime and other pop-culture';
 var fullSentence = processSentence(name, age, address, hobby);
+
 console.log(fullSentence);
 
